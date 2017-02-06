3f3ea8a78a0b687ad33052b43ba87ba84bc3f1f1
Anahit K <kostikyan.ana@gmail.com>
2017-02-06 13:53:36 -0800

    Hello (2/6/17)

--- random.js
+++ random.js
@@ -5,7 +5,16 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
-    
+    makeEllipse(85,70,5,15,"#4d4d4d",1)
+    makeEllipse(100,70,5,15,"#333333",1)
+    makeEllipse(114,70,5,15,"#4d4d4d",1)
+    makeEllipse(100,50,20,30,"#737373",1)
+    makeCircle(100,50,18,"#333333",1)
+    makeCircle(100,50,15,"#a3c2c2",1)
+    makeCircle
+    makeCircle(100,50,13,"#ebebb7",1)
+    makeCircle(100,50,12,"#efefc3",1)
+   
 }
 
 
@@ -13,7 +22,7 @@ function createFirstScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
-    
+    makeRect(50,50,10,10,"#669999",1)
 }
 
 
@@ -21,7 +30,7 @@ function createSecondScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
-    
+    makeCircle(50,50,50,"#993333",1)
 }
 
 
@@ -32,21 +41,22 @@ function createThirdScene() {
 // This function is called whenever you press the "Go!" button.
 function createRandomScene() {
     // Generate a random number between 0 and 1, and store it in a variable.
-    
+    var randomScene = Math.random()
     // If the number is less than 0.33, call the function to create your first scene.
-    
-    
+    if(randomScene < 0.33){
+    createFirstScene()
     
     // Else, if the number is less than 0.67, call the function to create your second scene.
-    
+    }else if(randomScene < 0.67){
+       createSecondScene() 
     
     
     // Else, call the function to create your third scene.
     
-    
-    
+    }else{
+    createThirdScene()
 }
-
+    }
 
 
 // DO NOT EDIT ANYTHING BELOW THIS LINE!
